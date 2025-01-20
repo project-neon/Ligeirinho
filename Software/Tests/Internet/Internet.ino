9c370a810dd2431e1dd1b8c5506167bdacd7d6ca
Ana Luiza Fioravante Di Sciascio <analuiza197@gmail.com>
2025-01-20 16:55:08 -0300

    chore[Internet.ino]: use ESPAsyncWebServer and clean code

--- Software/Tests/Internet/Internet.ino
+++ Software/Tests/Internet/Internet.ino
@@ -1,88 +1,53 @@
+// Referência: https://esp32io.com/tutorials/esp32-web-server#content_reading_the_sensor_value_from_esp32_via_web
 #include <WiFi.h>
+#include <ESPAsyncWebServer.h> //v3.1.0
+#include "SiteHtml.h"
+
+AsyncWebServer server(80);
+WiFiClient client;
 
 const char *ssid = "----";
 const char *password = "----";
 bool isLedOn = false;
-
-WiFiServer server(80);
-WiFiClient client;
-
-#include "SiteHtml.h"
-
 
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED_BUILTIN, OUTPUT);
 
-  // We start by connecting to a WiFi network
+  // Conectamos na internet
   Serial.println((String) "Connecting to: " + ssid);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
-    // if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-    //   Serial.printf("WiFi Failed!\n");
-    //   return;
-    // }
-
+  // Enquanto não conectar, printa pontinhos
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
 
+  // IP para acessar o servidor
   Serial.println("");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
+
+  // ENDPOINTS
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) {
+    Serial.println("GET /");
+    request->send(200, "text/html", webpage);
+  });
+
+  server.on("/led", HTTP_GET, [](AsyncWebServerRequest* request) {
+    Serial.println("GET /led");
+    isLedOn = !isLedOn;
+    digitalWrite(LED_BUILTIN, isLedOn);
+    request->send(200, "text/plain", String(isLedOn));
+  });
+
+  //Inicia o servidor
   server.begin();
 }
 
 void loop() {
-  client = server.accept();  // listen for incoming clients
-
-  if (client) {                     // if you get a client,
-    Serial.println("New Client.");  // print a message out the serial port
-    String currentLine = "";        // make a String to hold incoming data from the client
-    while (client.connected()) {    // loop while the client's connected
-      if (client.available()) {     // if there's bytes to read from the client,
-        char c = client.read();     // read a byte, then
-        Serial.print(c);
-        //Serial.write(c);            // print it out the serial monitor
-        if (c == '\n') {            // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println();
-
-            // the content of the HTTP response follows the header:
-            generateHtml();
-            // client.println(htmlString);
-            // The HTTP response ends with another blank line:
-            client.println();
-            // break out of the while loop:
-            break;
-          } else {  // if you got a newline, then clear currentLine:
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-
-        // Check to see if the client request was "GET /H" or "GET /L":
-        if (currentLine.endsWith("GET /led")) {
-          isLedOn = !isLedOn;
-          digitalWrite(LED_BUILTIN, isLedOn);  // GET /H turns the LED on
-        }
-        if (currentLine.endsWith("GET /L")) {
-          digitalWrite(LED_BUILTIN, LOW);  // GET /L turns the LED off
-        }
-      }
-    }
-    // close the connection:
-    client.stop();
-    Serial.println("Client Disconnected.");
-  }
+ 
 }
--- Software/Tests/Internet/SiteHtml.h
+++ Software/Tests/Internet/SiteHtml.h
@@ -1,55 +1,47 @@
-#include <WiFi.h>
-#include <string.h>
+//&#129302 = 🤖
 
-char ROBOT_EMOJI[] = "&#129302"; //🤖
+const char* webpage = R"=====(
+  <!DOCTYPE html>
+  <html style='text-align:center; background-color: #0db037'>
+    <head>
+      <!-- Define o nome da aba aberta -->
+      <title>Controle do Robo</title>
 
-void testFunc() {
-  Serial.println("Clicou");
-}
+      <!-- Define favicon https://stackoverflow.com/questions/59431371/use-emoji-as-favicon-in-websites-->
+      <link rel="icon" href="data:image/svg+xml,
+        <svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22>
+          <text y=%22.9em%22 font-size=%2290%22>  &#129302  </text>
+        </svg>">
+      
+      <h1> Controle do Robo &#129302 - NEON </h1>
+    </head>
+    <body>
 
-char *htmlList[] = {
-  "<!DOCTYPE html>",
-  "<html>",
-    "<head>",
-      //Define o nome da aba aberta
-      "<title>Controle do Robo</title>",
-      //Define favicon
-      "<link rel='icon' href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>", ROBOT_EMOJI, "</text></svg>'>",
-      "<h1> Controle do Robo", ROBOT_EMOJI, "- NEON </h1>",
-    "</head>",
-    "<body>",
-      "<script type='text/javascript'>",
-        "function myFunction() {",
-          "fetch('/led');",
-          "document.getElementById('demo').style.color = 'red';",
-          "console.log('Clicou');",
-        "}"
-      "</script>",
+      <script type='text/javascript'>
+        function myFunction() {
+          fetch('/led').then( response => response.text())
+            .then(isLedOn => {
+            isLedOn = isLedOn === '1';
 
+            document.getElementById('demo').textContent = isLedOn ? 'Desligar o LED' : 'Ligar o LED';
+            document.getElementById('circle').style.backgroundColor = isLedOn ?'blue' : 'lightGray';
+          });
+        }
+      </script>
 
-      "<button id='demo' onclick='myFunction()' >Clique LED</button> <br> <br>",
-      "<a href=\"/L\"> <button> Turn LED OFF </button> </a> <br> <br>",
-      "Click <a href=\"/L\">here</a> to turn the LED on pin 2 off.<br>",
-    "</body>",
-  "</html>"
-};
+      <style>
+        .dot {
+          height: 25px;
+          width: 25px;
+          background-color: lightGray;
+          border-radius: 50%;
+          display: inline-block;
+        }
+      </style>
+      
+      <button id='demo' onclick='myFunction()'> Ligar o LED </button> <br> <br>
+      <span id='circle' class="dot"></span>
 
-void generateHtml() {
-  int listSize = sizeof(htmlList)/sizeof(htmlList[0]);
-  for (int i = 0; i < listSize; i++) client.println(htmlList[i]);
-}
-
-
-
-
-
-//char htmlList[][100]
-
-// char htmlString[] = "Inicial";
-// void generateHtml() {
-//     int listSize = sizeof(htmlList)/sizeof(htmlList[0]);
-//     for (int i = 0; i < listSize; i++) strcat(htmlString, htmlList[i]);
-// } 
-
-//"<a href=\"/H\"> <button id='demo' onclick='myFunction()' > Turn LED ON </button> </a> <br> <br>",
-//"<a href=\"/L\"> <button> Turn LED OFF </button> </a> <br> <br>",
+    </body>
+  </html>
+  )=====";
